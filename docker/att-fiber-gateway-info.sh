a203f8a163e0b610e709db018baaaaf619b6d0af
Nathan Grennan <github@cygnusx-1.org>
2024-11-27 17:13:13 -0700

    Improving docker/att-fiber-gateway-info.sh

--- docker/att-fiber-gateway-info.sh
+++ docker/att-fiber-gateway-info.sh
@@ -6,16 +6,12 @@ if [ ! -z "${PASSWORD}" ]; then
   ARGS="${ARGS} -password ${PASSWORD}"
 fi
 
-if [ ! -z "${URL}" ]; then
-  ARGS="${ARGS} -url ${URL} "
-fi
-
-if [ ! -z "${PASSWORD}" ]; then
-  ARGS="${ARGS} -password ${PASSWORD}"
-fi
-
 if [ ! -z "${STATSDIPPORT}" ]; then
  ARGS="${ARGS} -statsdipport ${STATSDIPPORT}"
+fi
+
+if [ ! -z "${URL}" ]; then
+  ARGS="${ARGS} -url ${URL} "
 fi
 
 att-fiber-gateway-info ${ARGS}
